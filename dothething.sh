d2005a71441527b992be59c22fa3b4d195a26c97
Drew LeSueur <drewalex@gmail.com>
2025-05-03 22:59:59 -0700

    wip trying to build on mingw

--- dothething.sh
+++ dothething.sh
@@ -1,14 +1,52 @@
+#!/usr/bin/env bash
+
+# git clean -xdf
+# autoreconf -fvi
+# 
+# # Force the times‐header off + override GS_LIB_DEFAULT in one go:
+# # export CFLAGS="-DHAVE_SYS_TIMES_H=0 -DHAVE_FUNC_TIMES=0"
+# # export CPPFLAGS='-DGS_LIB_DEFAULT="\"/mingw64/share/ghostpdl\""' 
+# 
+# 
+# #  -O2 -Wall
+# export CFLAGS="-DHAVE_SYS_TIMES_H=0 -DHAVE_FUNC_TIMES=0 -DGS_LIB_DEFAULT=\"/mingw64/share/ghostpdl\""
+# 
+# ./configure --prefix=/mingw64
+# make -j"$(nproc)"
+# make install
+
+set -euo pipefail
+
+# 1) Clean out any old build artifacts
 git clean -xdf
+
+# 2) Re-gen the autotools scripts
 autoreconf -fvi
 
-# Force the times‐header off + override GS_LIB_DEFAULT in one go:
-# export CFLAGS="-DHAVE_SYS_TIMES_H=0 -DHAVE_FUNC_TIMES=0"
-# export CPPFLAGS='-DGS_LIB_DEFAULT="\"/mingw64/share/ghostpdl\""' 
+# 3) Disable <sys/times.h> and times() tests
+export CFLAGS="-DHAVE_SYS_TIMES_H=0 -DHAVE_FUNC_TIMES=0"
 
+# 4) Configure for a /mingw64 install
+./configure \
+  --prefix=/mingw64 \
+  --cache-file=/dev/null
 
-#  -O2 -Wall
-export CFLAGS="-DHAVE_SYS_TIMES_H=0 -DHAVE_FUNC_TIMES=0 -DGS_LIB_DEFAULT=\"/mingw64/share/ghostpdl\""
-
-./configure --prefix=/mingw64
+# 5) Build everything
 make -j"$(nproc)"
+
+# 6) Patch gconfigd.h: 
+#    - Replace the Windows‐style prefix (C:\msys64\mingw64) with /mingw64
+#    - Then turn all remaining backslashes into forward‐slashes
+sed -i \
+  -e 's#C:\\\\msys64\\\\mingw64#/mingw64#g' \
+  -e 's#\\\\#/#g' \
+  obj/gconfigd.h
+
+# 7) Recompile the two objects that pull in gconfigd.h
+make -j"$(nproc)" obj/gconfig.o obj/gscdefs.o
+
+# 8) Install into /mingw64
 make install
+
+echo "GhostPDL built & installed under /mingw64 with clean POSIX paths"
+
